2f106ce75efffa7cfe2ce991ccbbf323764b952f
Kian <knight7@gmail.com>
2024-05-22 10:56:04 +0900

    simple 1

--- api/index.py
+++ api/index.py
@@ -1,15 +1,10 @@
- 
 from http.server import BaseHTTPRequestHandler
-from os.path import dirname, abspath, join
-dir = dirname(abspath(__file__))
- 
+
+
 class handler(BaseHTTPRequestHandler):
- 
     def do_GET(self):
         self.send_response(200)
-        self.send_header('Content-type','text/plain')
+        self.send_header('Content-type', 'text/plain')
         self.end_headers()
-        with open(join(dir, '..', 'ai_webagent.py'), 'r') as file:
-          for line in file:
-            self.wfile.write(line.encode())
+        self.wfile.write(str('Hello World!!').encode())
         return
